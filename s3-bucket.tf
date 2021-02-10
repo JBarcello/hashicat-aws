fef91cc81ce20a3d7f53ea235ae4020084d41b9e
Jeremy Barcello <jbarcello@hashicorp.com>
2021-02-10 20:08:46 +0000

    changed naming to have no spaces

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/JeremyBarcello-Training/s3-bucket/aws"
   version = "1.17.0"
   # insert required variables here
-  bucket_prefix = "jeremy barcello"
+  bucket_prefix = "JBarcello"
 }
